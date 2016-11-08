2c1a68d44c07d69b794bfb4f49588430aa9aedc5
HaRdiSon <max.030298@mail.ru>
2016-11-08 10:01:17 +0200

    v0.004

---
+++ 2D Platformer Hero/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public float waitToRespawn;
+    public PlayerController thePlayer;
+	
+	void Start () {
+        thePlayer = FindObjectOfType<PlayerController>();
+	
+	}
+	
+	
+	void Update () {
+	
+	}
+
+    public void Respawn() {
+        StartCoroutine("RespawnCo");
+    }
+
+    public IEnumerator RespawnCo() {
+        thePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(waitToRespawn);
+        thePlayer.transform.position = thePlayer.respawnPosition;
+        thePlayer.gameObject.SetActive(true);
+    }
+}
