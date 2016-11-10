9b9c165c6eb8dd8e19094c70b5ba502963f23d19
HaRdiSon <max.030298@mail.ru>
2016-11-10 19:58:40 +0200

    v0.014

---
+++ 2D Platformer Hero/Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HurtPlayer : MonoBehaviour {
+
+    private LevelManager theLevelManager;
+    public int damageToGive;
+
+	void Start () {
+        theLevelManager = FindObjectOfType<LevelManager>();
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Player")
+        {
+            theLevelManager.HurtPlayer(damageToGive);
+        }
+    }
+}
