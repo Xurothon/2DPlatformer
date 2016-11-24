9e00183f7033975ae9fcee841cbea6fbaf7e0d8f
HaRdiSon <max.030298@mail.ru>
2016-11-24 18:42:26 +0200

    v0.045

---
+++ 2D Platformer Hero/Assets/Scripts/GameOver.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public string levelSelect;
+    public string mainMenu;
+    private LevelManager theLevelManager;
+
+	void Start () {
+        theLevelManager = FindObjectOfType<LevelManager>();
+	}
+	
+	void Update () {
+	
+	}
+
+    public void Restart() {
+        PlayerPrefs.SetInt("CoinCout", 0);
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.startingLives);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LevelSelect() {
+        PlayerPrefs.SetInt("CoinCout", 0);
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.startingLives);
+        SceneManager.LoadScene(levelSelect);
+    }
+
+    public void QuitToMainMenu() {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+}
---
+++ 2D Platformer Hero/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string firstLevel;
+    public string levelSelet;
+    public string[] levelNames;
+    public int startingLives;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    public void NewGame() {
+        SceneManager.LoadScene(firstLevel);
+        for (int i = 0; i < levelNames.Length; i++) {
+            PlayerPrefs.SetInt(levelNames[i], 0);
+        }
+        PlayerPrefs.SetInt("CoinCount", 0);
+        PlayerPrefs.SetInt("PlayerLives", startingLives);
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(levelSelet);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
